6607771e5e4e9b1ad04fcd2e4c5ab5f94cafc5f5
luye <luye19980902@163.com>
2020-04-27 21:48:44 +0800

    p2t1 page layouts

--- src/storage/page/hash_table_block_page.cpp
+++ src/storage/page/hash_table_block_page.cpp
@@ -17,30 +17,56 @@ namespace bustub {
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 KeyType HASH_TABLE_BLOCK_TYPE::KeyAt(slot_offset_t bucket_ind) const {
-  return {};
+  MappingType result;
+  memcpy(&result, array_ + bucket_ind, sizeof(MappingType));
+  return result.first;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 ValueType HASH_TABLE_BLOCK_TYPE::ValueAt(slot_offset_t bucket_ind) const {
-  return {};
+  MappingType result;
+  memcpy(&result, array_ + bucket_ind, sizeof(MappingType));
+  return result.second;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BLOCK_TYPE::Insert(slot_offset_t bucket_ind, const KeyType &key, const ValueType &value) {
-  return false;
+  if(IsOccupied(bucket_ind)) return false;
+
+  size_t idx = bucket_ind / 8;
+  size_t bit = bucket_ind % 8;
+  occupied_[idx] |= (1<<bit);
+  readable_[idx] |= (1<<bit);
+
+  MappingType tmp = MappingType(key, value);
+  memcpy(array_+bucket_ind, &tmp, sizeof(MappingType));
+  return true;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
-void HASH_TABLE_BLOCK_TYPE::Remove(slot_offset_t bucket_ind) {}
+void HASH_TABLE_BLOCK_TYPE::Remove(slot_offset_t bucket_ind) {
+  size_t idx = bucket_ind / 8;
+  size_t bit = bucket_ind % 8;
+  if(idx >= sizeof(occupied_)/sizeof(std::atomic_char)) return;
+  readable_[idx] &= ~(1<<bit);
+}
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BLOCK_TYPE::IsOccupied(slot_offset_t bucket_ind) const {
-  return false;
+  size_t idx = bucket_ind / 8;
+  size_t bit = bucket_ind % 8;
+  if(idx >= sizeof(occupied_)/sizeof(std::atomic_char)) return false;
+  bool result = occupied_[idx] & (1<<bit);
+  return result;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BLOCK_TYPE::IsReadable(slot_offset_t bucket_ind) const {
-  return false;
+  size_t idx = bucket_ind / 8;
+  size_t bit = bucket_ind % 8;
+  if(idx >= sizeof(readable_)/sizeof(std::atomic_char)) return false;
+  bool result = readable_[idx] & (1<<bit);
+  return result;
 }
 
 // DO NOT REMOVE ANYTHING BELOW THIS LINE
